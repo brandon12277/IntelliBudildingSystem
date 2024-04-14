98dc1c433085b7391cba41ed84974e4a8d7cdd4c
manideepa-shaw <78009996+manideepa-shaw@users.noreply.github.com>
2024-04-14 12:38:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AdminLogin from './components/admin_login';
 import Admin from './components/admin';
+import Landing from './components/landing';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       
       <Router>
        <Routes>
+        <Route path="/" element={ <Landing /> } />
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<Admin />} />
 
